6ec62eb25359557b5295f8b418668aaa0cbe2d79
Андрей Исаченко <isachenko.a@sant-on.ru>
2023-08-10 12:24:35 +0800

    Исправлена проверка CheckRegion, а так же добавлен метод CheckHasWords

--- TestBook/GetmethodTest/GetMethodsTest.cs
+++ TestBook/GetmethodTest/GetMethodsTest.cs
@@ -25,6 +25,7 @@ namespace TestBook.Getmethod
         }
         [TestCase("Dorne", "true")]
         [TestCase("Dorne", "false")]
+        [TestCase("Dorne", "564")]
         [TestCase("The Westerlands", "true")]
         public async Task PositiveGetBook(string region, string hasWords)
         {
@@ -55,31 +56,49 @@ namespace TestBook.Getmethod
             var jsonResponse = await getResponse.Content.ReadAsStringAsync();
             //Так как в ответ получаем массив объектов, парсим в Jarray
             JArray arrayObject = JArray.Parse(jsonResponse);
-            //Сравниваем объекты массива со схемоу
+            //Сравниваем объекты массива со схемой
             var isValid = arrayObject.IsValid(schema);
             Assert.Multiple(() =>
             {
+                Assert.IsTrue(CheckHasWords(hasWords, arrayObject));
                 Assert.IsTrue(CheckRegion(region, arrayObject));
                 Assert.That(getResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
                 Assert.That(isValid, Is.True);
 
             });
         }
+        //Проверка что все элементы массивы содержат поле регион с нужным значением
         private static bool CheckRegion(string region, JArray arrayObject) 
         {
-
-            foreach (JObject item in arrayObject)
+            //Возвращаем true если все элементы содержат region с заданным значением и false если не все
+            bool checkRegion = arrayObject.All(item => item.SelectToken("region").ToString() == region);
+            return checkRegion;
+        }
+        //Проверка строки words
+        private static bool CheckHasWords(string hasWords, JArray arrayObjecct)
+        {
+            //Проверяем что строка не пустая если значение True
+            if (hasWords == "true" || hasWords == "True")
             {
-                //Находим у объектов массива значение региона
-                var regionValue = item.GetValue("region");
-                //Сравниваем значение с тем которое было задано
-                if (regionValue.ToString() == region)
-                {
-                    return true;
-                }
-                return false;
+                //Возвращаем true если все элементы содержат words с не пустой строкой и false если не все
+                bool checkHasWords = arrayObjecct.All(item => item.SelectToken("words").ToString() != "");
+                return checkHasWords;
             }
-            return false;
+            //Проверяем что строка пустая при значении False
+            else if (hasWords == "false" || hasWords == "False")
+            {
+                //Возвращаем true если все элементы содержат words с пустой строкой и false если не все
+                bool checkHasWords = arrayObjecct.All(item => item.SelectToken("words").ToString() == "");
+                return checkHasWords;
+            }
+            else
+            {
+                //Если в параметр hasWords передать любое значение отличное от bool то, выдаёт и пустые
+                //и не пустые words соотвественно в случае если у нас не True и не False просто проверяем
+                //что у words есть какое то значение
+                bool checkHasWords = arrayObjecct.All(item => item.SelectToken("words").ToString() != null);
+                return checkHasWords;
+            }
         }
         //Маленький негативный кейс
         [TestCase(5100, ExpectedResult = HttpStatusCode.NotFound)]
